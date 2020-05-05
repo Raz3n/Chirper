176f40310b248ec7bc6eac27457affc5cbbf9413
Nelson <nbritob@hotmail.com>
2020-05-05 16:13:21 +0100

    adds chirp_detail_view model and url path

--- chirper/urls.py
+++ chirper/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from django.urls import path
 
-from chirper.chirps.views import home_view
+from chirps.views import home_view, chirp_detail_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home_view),
+    path('chirps/<int:chirp_id>', chirp_detail_view),
 ]
--- chirps/views.py
+++ chirps/views.py
@@ -1,6 +1,17 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404, JsonResponse
 from django.shortcuts import render
+
+from .models import Chirp
 
 # Create your views here.
 def home_view(request, *args, **kwargs):
     return HttpResponse('<h1>Hello World<h1>')
+
+
+def chirp_detail_view(request, chirp_id, *args, **kwargs):
+    try:
+        obj = Chirp.objects.get(id=chirp_id)
+    except:
+        raise Http404
+    return HttpResponse(f'<h1>Hello {chirp_id} - {obj.content} <h1>')
+    
