366cc6e1687a7438a3f5feb890bc5b44b66bc9ba
Nelson <nbritob@hotmail.com>
2020-05-08 13:30:03 +0100

    adds home template

--- chirps/views.py
+++ chirps/views.py
@@ -5,18 +5,18 @@ from .models import Chirp
 
 # Create your views here.
 def home_view(request, *args, **kwargs):
-    return HttpResponse('<h1>Hello World<h1>')
+    return render(request, "pages/home.html", context={}, status=200)
 
 
 def chirp_detail_view(request, chirp_id, *args, **kwargs):
-    try:
-        obj = Chirp.objects.get(id=chirp_id)
-    except:
-        raise Http404
     data = {
         "id": chirp_id,
-        "content": obj.content,
-        #"image_path": obj.image.ul
-        
     }
-    return JsonResponse(data)
+    status = 200
+    try:
+        obj = Chirp.objects.get(id=chirp_id)
+        data['content'] = obj.content
+    except:
+        data['message'] = "Not found"
+        status = 404
+    return JsonResponse(data, status=status)
