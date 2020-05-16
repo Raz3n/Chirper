bc7c1b074028a220faf3a20a359782e7d0830c87
Nelson <nbritob@hotmail.com>
2020-05-16 23:41:33 +0100

    adds ChirpLike class in models and adds timestamps on chirps

--- chirps/models.py
+++ chirps/models.py
@@ -4,12 +4,19 @@ from django.db import models
 
 User = settings.AUTH_USER_MODEL
 
+class ChirpLike(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    chirp = models.ForeignKey("Chirp", on_delete=models.CASCADE)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
 class Chirp(models.Model):
     # id = models.AutoField(primary_key=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
+    likes = models.ManyToManyField(User, related_name='chirp_user', blank=True, through=ChirpLike)
     content = models.TextField(blank=True, null=True)
     image = models.FileField(upload_to='images/', blank=True, null=True)
-    
+    timestamp = models.DateTimeField(auto_now_add=True)
+
     # def __str__(self): this would show the content of the chirp in the admin.
     #     return self.content
     
