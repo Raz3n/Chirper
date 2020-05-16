52e28fc40228bb4021d58c3faf54591bcc33153e
Nelson <nbritob@hotmail.com>
2020-05-16 20:20:10 +0100

    adds permissions and authentication classes decorators

--- chirps/views.py
+++ chirps/views.py
@@ -4,7 +4,9 @@ from django.http import HttpResponse, Http404, JsonResponse
 from django.shortcuts import render, redirect
 from django.utils.http import is_safe_url
 
-from rest_framework.decorators import api_view
+from rest_framework.authentication import SessionAuthentication
+from rest_framework.decorators import api_view, authentication_classes, permission_classes
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from .models import Chirp
 from .forms import ChirpForm
@@ -17,6 +19,8 @@ def home_view(request, *args, **kwargs):
     return render(request, "pages/home.html", context={}, status=200)
 
 @api_view(['POST']) #http method the client sends == post
+# @authentication_classes([SessionAuthentication, MyCustomAuth])
+@permission_classes([IsAuthenticated])
 def chirp_create_view(request, *args, **kwargs):
     serializer = ChirpSerializer(data = request.POST or None)
     if serializer.is_valid(raise_exception=True):
