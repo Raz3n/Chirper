8b15530811bfb53292e7be85468eb77f8f87be71
Nelson <nbritob@hotmail.com>
2020-05-18 15:12:28 +0100

    finishes chirp action view

--- chirps/serializers.py
+++ chirps/serializers.py
@@ -4,10 +4,18 @@ from .models import Chirp
 
 
 MAX_CHIRP_LENGTH = settings.MAX_CHIRP_LENGTH
+CHIRP_ACTION_OPTIONS = settings.CHIRP_ACTION_OPTIONS
+
 
 class ChirpActionSerializer(serializers.Serializer):
     id = serializers.IntegerField()
     action = serializers.CharField()
+    
+    def validate_action(self, value):
+        value = value.lower().strip()
+        if not value in CHIRP_ACTION_OPTIONS:
+            raise serializers.ValidationError("This is not a valid action for chirps.")
+        return value
     
 class ChirpSerializer(serializers.ModelSerializer):
     class Meta:
