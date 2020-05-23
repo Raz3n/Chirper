4feab738b51c840fb0169278b7d43bc55a7776cb
Nelson <nbritob@hotmail.com>
2020-05-23 14:45:21 +0100

    react is now being rendered by django

---
+++ chirper-web/build/precache-manifest.08605b29b4f43145c18e5275d79a28b5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b431ef053fa4d63c246937bc6777a135",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7505b60dd4c774755b1c",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "f96c4b7f67b806921b47",
+    "url": "/static/js/2.c060739c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.c060739c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7505b60dd4c774755b1c",
+    "url": "/static/js/main.cdf35232.chunk.js"
+  },
+  {
+    "revision": "81defbef66eca2989dd9",
+    "url": "/static/js/runtime-main.4716b529.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- chirper/urls.py
+++ chirper/urls.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
+from django.conf import settings
+from django.conf.urls.static import static
+from django.views.generic import TemplateView
+
 
 from chirps.views import (
     home_view,
@@ -13,8 +17,12 @@ from chirps.views import (
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home_view),
+    path('react/', TemplateView.as_view(template_name='react.html')),
     path('create-chirp', chirp_create_view),
     path('chirps', chirp_list_view),
     path('chirps/<int:chirp_id>', chirp_detail_view),
     path('api/chirps/', include('chirps.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
