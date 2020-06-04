6bed79bb133ff017e991ec7c04c695aa8a33b8a0
Nelson <nbritob@hotmail.com>
2020-06-04 15:49:37 +0100

    adds login, logout and registration to views in accounts and adds accounts to templates

--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, logout
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 
 # Create your views here.
 
 def login_view(request, *args, **kwargs):
-    
-    return render(request, "form.html", {})
+    form = AuthenticationForm(request, data=request.POST or None)
+    if form.is_valid():
+        user_ = form.get_user()
+        login(request, user_)
+        return redirect("/")
+    return render(request, "accounts/login.html", {"form": form})
+
+def logout_view(request, *args, **kwargs):
+    if request.method == "POST":
+        logout(request)
+        return redirect("/login")
+    return render(request, "accounts/logout.html")
+
+def registration_view(request, *args, **kwargs):
+    form = UserCreationForm(request.POST or None)
+    if form.is_valid():
+        print(form.cleaned_data)
+    return render(request, "accounts/register.html", {"form": form})
