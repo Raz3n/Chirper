0c0342e1452cd69745760921f548ba457c3b11d9
Nelson <nbritob@hotmail.com>
2020-06-06 18:52:16 +0100

    adds profile_update_view in profile view

--- profiles/urls.py
+++ profiles/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-
+from .forms import ProfileForm
 from .views import profile_detail_view
 
 urlpatterns = [
--- profiles/views.py
+++ profiles/views.py
@@ -1,6 +1,7 @@
 from django.http import Http404
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
+from .forms import ProfileForm
 from .models import Profile
 
 def profile_detail_view(request, username, *args, **kwargs):
@@ -13,3 +14,26 @@ def profile_detail_view(request, username, *args, **kwargs):
         "profile": profile_obj
     }
     return render(request, "profiles/detail.html", context)
+
+def profile_update_view(request, *args, **kwargs):
+    if not request.user.is_authenticated:
+        return redirect("/login?next=/profile/update")
+    user = request.user
+    my_profile = user.profile
+    form = ProfileForm(request.POST or None, instance=my_profile)
+    if form.is_valid:
+        profile_obj = form.save(commit=False)
+        first_name = form.cleaned_data.get('first_name')
+        last_name = form.cleaned_data.get('last_name')
+        email_address = form.cleaned_data.get('email_address')
+        user.first_name = first_name
+        user.last_name = last_name
+        user.email_address = email_address
+        user.save()
+        profile_obj.save()
+    context = {
+        "form": form,
+        "btn_label": "Save",
+        "title": "Update profile"
+    }
+    return render(request, "profiles/form.html", context)
