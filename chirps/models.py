c24c89ed3c62985f1a2a03b5f909e8af820dfebb
Nelson <nbritob@hotmail.com>
2020-06-07 16:15:37 +0100

    adds related name to chirp model and adds chirp related name test

--- chirps/models.py
+++ chirps/models.py
@@ -12,7 +12,7 @@ class ChirpLike(models.Model):
 class Chirp(models.Model):
     # id = models.AutoField(primary_key=True)
     parent = models.ForeignKey("self", null=True, on_delete=models.SET_NULL) # Chirp wont have a parent until it is rechirped
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="chirps")
     likes = models.ManyToManyField(User, related_name='chirp_user', blank=True, through=ChirpLike)
     content = models.TextField(blank=True, null=True)
     image = models.FileField(upload_to='images/', blank=True, null=True)
--- profiles/models.py
+++ profiles/models.py
@@ -4,6 +4,7 @@ from django.db.models.signals import post_save
 
 # Create your models here.
 User = settings.AUTH_USER_MODEL
+
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     location = models.CharField(max_length=220, blank=True, null=True)
